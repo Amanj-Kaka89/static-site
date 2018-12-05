c2a315e6901be3b81a1ec1733fc081b9ace87479
Amanj Kaka <amanj_kakaei@yahoo.com>
2018-12-05 12:28:46 +0700

    modify form

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -4,14 +4,24 @@ import '../stylesheets/style.scss';
 import * as config from '../../config/config.json';
 
 const form = document.querySelector('.form');
+const name = document.querySelector('.name');
+const email = document.querySelector('.email');
+const comment = document.querySelector('.comment');
 
 // ðŸ‘‡ Write homework code here
 const formData = {
-  name: 'Amanj',
-  email: 'your@email.com',
-  comment: 'This is my comment',
-  priority: document.querySelector('input[name="priority"]:checked'),
+  name: name.value,
+  email: email.value,
+  comment: comment.value,
+  priority: document.querySelector('input[name="priority"]:checked').value,
   topic: [...document.querySelectorAll('input[type="checkbox"]:checked')].map(v => v.value),
+};
+const clearForm = () => {
+  name.value = '';
+  email.value = '';
+  comment.value = '';
+  document.querySelector('input[name = "priority"]:checked').checked = false;
+  [...document.querySelectorAll('input[type = "checkbox"]:checked')].map(v => (v.checked = false));
 };
 // ðŸ‘† Write homework code here
 
@@ -21,6 +31,9 @@ const handleSubmit = e => {
     .post(config.site.url, qs.stringify({ 'form-name': 'Comments', ...formData }, { arrayFormat: 'brackets' }))
     .then(_ => console.log('success'))
     .catch(_ => console.log('failed'));
+    .then(_ => {
+  clearForm();
+});
 };
 
 form.addEventListener('submit', handleSubmit);
