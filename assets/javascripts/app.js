ead3106d1ed5f3a5853640c1ead6df911134281c
Amanj Kaka <amanj_kakaei@yahoo.com>
2018-12-05 09:54:53 +0700

    commit form

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,3 +1,26 @@
+import qs from 'qs';
+import axios from 'axios';
 import '../stylesheets/style.scss';
+import * as config from '../../config/config.json';
 
-console.log('Hello World');
+const form = document.querySelector('.form');
+
+// ðŸ‘‡ Write homework code here
+const formData = {
+  name: 'Amanj',
+  email: 'your@email.com',
+  comment: 'This is my comment',
+  priority: 'Medium',
+  topic: ['HTML', 'CSS', 'Javascriot', 'Design'],
+};
+// ðŸ‘† Write homework code here
+
+const handleSubmit = e => {
+  e.preventDefault();
+  axios
+    .post(config.site.url, qs.stringify({ 'form-name': 'Comments', ...formData }, { arrayFormat: 'brackets' }))
+    .then(_ => console.log('success'))
+    .catch(_ => console.log('failed'));
+};
+
+form.addEventListener('submit', handleSubmit);
